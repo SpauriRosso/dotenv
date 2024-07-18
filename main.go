fbabb44252f9f2c13cc77311b879c754838e28f1
SpyRisk <46430869+SpauriRosso@users.noreply.github.com>
2024-07-18 23:09:33 +0200

    OHNON
    my pc out of battery and i forgot my charger at school so i hope i can push before my pc actually dies.

--- main.go
+++ main.go
@@ -1,1 +1,37 @@
-package dotenv
+package main
+
+import (
+	"os"
+	"strings"
+)
+
+var Env string
+var File []byte
+
+//func main() {
+//	Define(".env")
+//	fmt.Println(GetEnv("SUPER_SECRET_KEY"))
+//}
+
+func Define(path string) any {
+	_, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	Env = path
+	return Env
+}
+
+func GetEnv(key string) any {
+	var value string
+	File, _ = os.ReadFile(Env)
+
+	for _, v := range strings.Split(string(File), "\n") {
+		if strings.HasPrefix(v, key+"=") {
+			value = strings.TrimPrefix(v, key+"=")
+			value = strings.Trim(value, "\n")
+			break
+		}
+	}
+	return value
+}
---
+++ settings.go
@@ -0,0 +1,11 @@
+package main
+
+var (
+//Env string
+
+/*
+Maybe for future feature
+Yaml string
+Json string
+*/
+)
